79c0779b539f1896d9b35e4215bb643be6e6166d
ashrafJebril <ashrafjebril9@gmail.com>
2019-01-26 19:47:17 +0200

    secound

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,26 +5,26 @@ import { Link } from "react-router-dom";
 const Home = () => {
     return (
 
-        <div className="container">
-            <div className="row all">
+       
+            <div className="row ">
      
-                <div className="row">
-                    <div className="col-lg-5">
+              
+                    <div className="col-lg-5 col-sm-5">
                         <img src={img} className="home-img"></img>
                     </div>
-                    <div className="col-lg-6">
+                    <div className="col-lg-7 col-sm-7">
                         <h1>Hello, </h1><span>A bit About Me</span>
-                        <div className="row">  <Link to={{pathname:"/Resume"}}> <div className="col-lg-2 red" > My Resume</div></Link>  
+                        <div className="row">  <Link to={{pathname:"/Resume"}}> <div className="col-lg-2  red col-sm-2" > My Resume</div></Link>  
                          <Link to={{pathname:"/Projects"}}> <div className="col-lg-2 blue" >My Work</div></Link>  
                             <Link to={{pathname:"/Skills"}}>   <div className="col-lg-2 yellow">My Skills</div></Link>
 
                         </div>
-                     <div className="row">
-                     <div className="col-lg-10"> I am a passionate with front end specially , strong in design , good problem solving skills , always seek to learn more </div></div>
-                    </div>
-                </div>
+                  
+                     <div className="col-lg-10 col-sm-10"> I am a passionate with front end specially , strong in design , good problem solving skills , always seek to learn more </div></div>
+            
+         
             </div>
-        </div>
+      
     )
 
 }
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,37 +1,47 @@
-import React from "react"
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
-    return (
-        <div className="container nav all" >
-            <div className="row">
-                <div className="col-lg-12">
-                    <h2>Ashraf Jebril</h2>
-                </div></div>
-            <div className="row des">
-                <div className="col-lg-4">
-                    <p>Full Stack Software Engineer</p>
-                </div>
-                <div className="col-lg-8">
-                    <ul><li>
-                        <Link
-                            to={{
-                                pathname: "/"
-                            }}
-                        >
-
-                            Home</Link></li><li> <Link to={{ pathname: "/Projects" }}>Projects </Link></li><li>            <Link
-                                to={{
-                                    pathname: "/Intereists"
-                                }}
-                            >
-
-                                Intereists </Link></li> </ul>
-
-                </div>
-            </div>
+  return (
+    <div className=" nav ">
+      <div className="row">
+        <div className="col-lg-5 ">
+          <h2>Ashraf Jebril</h2>
         </div>
-
-    )
-}
-export default Nav
+      </div>
+      <div className="row des">
+        <div className="col-lg-5">
+          <p>Full Stack Software Engineer</p>
+        </div>
+        <div className="col-lg-7">
+          <ul>
+            <li>
+              <Link
+                to={{
+                  pathname: "/"
+                }}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link to={{ pathname: "/Projects" }}>Projects </Link>
+            </li>
+            <li>
+              {" "}
+              <Link
+                to={{
+                  pathname: "/Intereists"
+                }}
+              >
+                Intereists{" "}
+              </Link>
+            </li>{" "}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Nav;
